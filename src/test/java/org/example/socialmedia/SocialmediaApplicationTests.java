e01a54112addf3a8742ca864363a43e4d9790303
solangemuk41-alt <solangemuk41@gmail.com>
2026-06-22 16:50:40 +0200

    Project structure

---
+++ src/test/java/org/example/socialmedia/SocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
